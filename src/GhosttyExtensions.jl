428e198416492ab7e1281569be8cd0b08d0cc9af
Peter Pieske <71228733+piechologist@users.noreply.github.com>
2025-05-03 22:48:49 +0200

    Add experimental support for the Kitty Keyboard Protocol
    On startup, GhosttyExtensions.jl emits an escape sequence that asks the terminal to use the Kitty Keyboard Protocol. This allows more flexibility to bind keys. Particularly, the command key (macOS) or hyper key (Linux) can be bound.
    
    Julia's REPL does not understand that protocol and we need to translate some Kitty escape sequences to legacy encodings. This is done by a simple table.

--- src/GhosttyExtensions.jl
+++ src/GhosttyExtensions.jl
@@ -26,7 +26,7 @@ include("shellintegration.jl")
 # swallow the first part of certain bindings and the remaining part will leak into the
 # terminal. We need to add wildcards for these bindings to let them pass through.
 # See `LineEdit.prefix_history_keymap` for the default wildcards.
-const extra_keymap = Dict{Any,Any}(
+const extra_keymap = Base.AnyDict(
     "\eOP" => (s, o...) -> invoke_help(s), # F1
     "\eOQ" => (s, o...) -> parenthesize(s), # F2
     "\e[24~" => (s, o...) -> toggle_prefix(s, "@time"), # F12
@@ -42,13 +42,47 @@ const extra_keymap = Dict{Any,Any}(
     # Shift-Command-Right/Left:
     "\e[1;10C" => (s, o...) -> select_to_end_of_line(s),
     "\e[1;10D" => (s, o...) -> select_to_start_of_line(s),
+    # Kitty Keyboard Protocol:
+    "\e[99;9u" => (s, o...) -> copy_region(s), # Command-C
+    "\e[120;9u" => (s, o...) -> cut_region(s), # Command-X
 )
 
-const extra_wildcards = Dict{Any,Any}(
-    "\e[24~" => "*",    # F12
-    "\e[24;2~" => "*",  # Shift-F12
-    "\e[1;4*" => "*",   # Shift-Option-ArrowKeys
-    "\e[1;10*" => "*",  # Shift-Command-ArrowKeys
+
+const extra_wildcards = merge!(
+    Base.AnyDict(
+        "\e[24~" => "*",    # F12
+        "\e[24;2~" => "*",  # Shift-F12
+        "\e[1;4*" => "*",   # Shift-Option-ArrowKeys
+        "\e[1;10*" => "*",  # Shift-Command-ArrowKeys
+    ),
+    # Kitty Keyboard Protocol:
+    Base.AnyDict("\e[$(n);5u" => "*" for n in 97:122), # ^A..^Z
+    Base.AnyDict("\e[$(c)" => "*" for c in 'P':'S'),   # F1..F4
+    Base.AnyDict(
+        "\e[27u" => "*",    # Escape
+        "\e[99;9u" => "*",  # Command-C
+        "\e[120;9u" => "*", # Command-X
+    ),
+)
+
+# This table translates problematic escape sequences from the Kitty Keyboard Protocol to the
+# REPL's legacy encoding. The other key bindings should work out of the box.
+#
+# The Kitty Keyboard Protocol
+# https://sw.kovidgoyal.net/kitty/keyboard-protocol/
+#   print("\e[>1u") # turn it on
+#   print("\e[<u") # turn it off
+const kitty_escape_defaults = merge!(
+    Base.AnyDict("\e[$(n);5u" => LineEdit.KeyAlias('^' * Char(n - 32)) for n in 97:122),
+    Base.AnyDict("\e[$(c)" => LineEdit.KeyAlias("\eO$c") for c in 'P':'S'),
+    Base.AnyDict(
+        "\e[9;2u" => LineEdit.KeyAlias("\e[Z"),   # Shift-Tab
+        "\e[9;3u" => LineEdit.KeyAlias("\e[Z"),   # Option-Tab
+        "\e[13;3u" => LineEdit.KeyAlias("\e\r"),  # Option-Return
+        "\e[127;3u" => LineEdit.KeyAlias("\e\b"), # Option-Backspace
+        "\e[120;5u" => LineEdit.KeyAlias("^X^X"), # ^X (this keeps the REPL from crashing)
+        "\e[27u" => LineEdit.KeyAlias("^G"),      # Escape (cancel a selection)
+    ),
 )
 
 """
@@ -87,6 +121,8 @@ function __init__()
             end
 
             # Set up the REPL with the custom key bindings.
+            print("\e[>1u") # tell the terminal to use the Kitty Keyboard Protocol
+            merge!(LineEdit.escape_defaults, kitty_escape_defaults)
             merge!(LineEdit.prefix_history_keymap, extra_wildcards)
             repl.interface = REPL.setup_interface(repl; extra_repl_keymap=extra_keymap)
 
